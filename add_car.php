1b951ab0b9e8bd79ba4ac250179d78e568d1d179
Natniif <fintan.hardy@gmail.com>
2023-09-11 15:47:17 +0100

    fixed merge changes

--- add_car.php
+++ add_car.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once "../vendor/autoload.php";
-require_once "utils.php";
+require_once "vendor/autoload.php";
+require_once "src/utils.php";
 
 use CarStore\UpdateCars;
 use CarStore\Car;
