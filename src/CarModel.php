c9ba8cfebd6292677c420a46ef43a54214c9ce6f
Natniif <fintan.hardy@gmail.com>
2023-09-14 12:30:26 +0100

    simplified

--- src/CarModel.php
+++ src/CarModel.php
@@ -19,16 +19,15 @@ class CarModel
     public function getAllCarInfo(): array
     {
         $query = $this->db->prepare(
-            "SELECT `name`, `year_made`, `zero_sixty`, `price`, `brand` FROM `cars` WHERE `deleted` = 0"
+            "SELECT `id`, `name`, `year_made`, `zero_sixty`, `price`, `brand` FROM `cars` WHERE `deleted` = 0"
         );
         $query->execute();
         $cars = $query->fetchAll();
 
         $cars_ret = [];
-        $method = new CarModel(make_db());
 
         foreach ($cars as $car) {
-            $cars_ret[] = new Car($method->getIdFromName($car['name']), $car["name"], $car["year_made"], $car["zero_sixty"], $car["price"], $car["brand"]);
+            $cars_ret[] = new Car($car["id"], $car["name"], $car["year_made"], $car["zero_sixty"], $car["price"], $car["brand"]);
         }
 
         return $cars_ret;
